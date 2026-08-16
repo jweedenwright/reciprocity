0653f630eea57901db8762155006114acc0eff4f
jweedenwright <jweedenwright@navigatingcancer.com>
2026-08-16 14:13:45 -0500

    Cook mode complete

--- website/js/cookbook.js
+++ website/js/cookbook.js
@@ -1,25 +1,62 @@
 // Used to pull all recipes from our Google endpoint
-const recipes = []
+let recipes = []
+// Initialize wake lock and elements
+let wakeLock = null;
+const recipeContainer = document.getElementById('main-content');
+const cookModeBtn = document.getElementById('cook-mode');
+console.log(cookModeBtn)
+
+cookModeBtn.addEventListener('click', async () => {
+  if (!wakeLock) {
+    await enableCookMode();
+  } else {
+    await disableCookMode();
+  }
+});
+
+document.addEventListener('visibilitychange', async () => {
+  if (wakeLock !== null && document.visibilityState === 'visible') {
+    wakeLock = await navigator.wakeLock.request('screen');
+  }
+});
+
+async function enableCookMode() {
+  try {
+    wakeLock = await navigator.wakeLock.request('screen');
+    recipeContainer.classList.add('cook-mode-active');
+    console.log("add class");
+    cookModeBtn.classList.add('btn-danger');
+  } catch (err) {
+    console.error(`${err.name}, ${err.message}`);
+  }
+}
+
+async function disableCookMode() {
+  if (wakeLock) {
+    await wakeLock.release();
+    wakeLock = null;
+  }
+  recipeContainer.classList.remove('cook-mode-active');
+  cookModeBtn.classList.remove('btn-danger');
+}
+
 async function getCookbook() { 
     try {
         console.log("Fetching: " + config.api + 'api/cookbook');
         const response = await axios.get(config.api + 'api/cookbook');
         if (response.status !== 200) throw new Error('Network error')
         const rawData = await response.data // Or response.json(), depending on your API structure
-        app.config.globalProperties.$recipes = rawData;
+        recipes = rawData;
         return true;
     } catch (error) {
         console.error('Failed to fetch HTML:', error)
-        app.config.globalProperties.$hasError.value = true
         return false;
-    } finally {
-        app.config.globalProperties.$isLoading.value = false
     }
 };
 
 // Used to load all recipes into the UI
 async function showCookbook() {
-    app.config.globalProperties.$recipes.forEach((item) => {
+    recipes.forEach((item) => {
         var li = "<li class='list-group-item justify-content-between align-items-center recipe' tag='a' href='#/recipe/" + item.id + "' data-id='" + item.id + "' action>";
         li += "<div class='d-flex'><h3 class='fw-bold'>" + DOMPurify.sanitize(item.name).replace('.md','') + "</h3></div><div class='d-flex'><p>";
         item.description.split('|').forEach(tag => {
